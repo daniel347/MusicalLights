4e1ab001e1b501f387bf8ed67ca6b701f9fc59b4
Daniel Siromani <daniel.siromani@gmail.com>
2019-12-16 00:00:53 +0000

    Auto gain adjustment working though fourier gain still exists

--- source/music_lights_main.py
+++ source/music_lights_main.py
@@ -10,7 +10,7 @@ BLOCK_DUR = 100  # by default, take 50ms chunks and feed them into the Fourier t
 
 light_freq_ranges = [(50, 200), (200, 300), (300, 400), (400, 650), (650, 1000)]
 N_lights = len(light_freq_ranges)
-MAX_FOURIER = 650  # TODO : dynamic gain adjustment - the required gain seems to vary a lot based on the music
+MAX_FOURIER = 255  # TODO : unify gains into only the one adaptive one
 lights = [light.Light(1, r, MAX_FOURIER, 0) for r in light_freq_ranges]  # make a light class instance for every frequency range
 tree = sim.ChristmasTreeSim(N_lights)  # start the simulation of the tree
 
@@ -18,9 +18,9 @@ brightnesses = [10] * N_lights
 update_display = False
 
 audio_gain = 10
-P_COEFF = 100
-TARGET_LEVEL = 1e2
-AUTOGAIN_T = 10  # timeframe over which averages are taken for auto gain adjustment
+P_COEFF = 1
+TARGET_LEVEL = 3
+AUTOGAIN_T = 10  # time frame in seconds over which averages are taken for auto gain adjustment
 sound_amplitudes = np.zeros(np.floor((AUTOGAIN_T * 1000) / BLOCK_DUR).astype(int))  # circular buffer of arrays
 
 def callback(indata, frames, time, status):
@@ -28,13 +28,15 @@ def callback(indata, frames, time, status):
     global update_display
 
     if any(indata):
-        fft, freqs = dsp.fourier(indata[:, 0], samplerate, gain=1000)
+        amplified_audio = np.array(indata[:, 0]) * audio_gain
+        fft, freqs = dsp.fourier(amplified_audio, samplerate)
         # fft contains the fourier coefficients
         # freqs is an array of the corresponding frequencies
 
         bins = dsp.bin_spectrum(fft, freqs, light_freq_ranges)
 
-        beat = dsp.detect_beat(indata[:, 0], 0.3)
+        beat = dsp.detect_beat(amplified_audio, 0.3)
+        update_gain(dsp.rms(amplified_audio))
 
         for light_obj, bin_val in zip(lights, bins):
             light_obj.set_brightness(bin_val, beat)  # saves data to light instance and also does scaling and constraining
@@ -43,14 +45,15 @@ def callback(indata, frames, time, status):
     else:
         print('no input', flush=True)
 
-def update_gain(rms_amplitude, ):
+def update_gain(rms_amplitude):
     """function for adaptive audio gain"""
+    global audio_gain
 
     # first add the new amplitude to the circular buffer
     sound_amplitudes[0] = rms_amplitude
     np.roll(sound_amplitudes, 1)
 
-    print(dsp.rms(sound_amplitudes))
+    audio_gain += (TARGET_LEVEL - dsp.rms(sound_amplitudes)) * P_COEFF
 
 
 if __name__ == "__main__":
