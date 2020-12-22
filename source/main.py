3c33bce5db54911625437a778bb39dc073310a28
Daniel Siromani <daniel.siromani@gmail.com>
2020-12-22 11:28:07 +0000

    Cleaned up main a bit

--- source/main.py
+++ source/main.py
@@ -1,24 +1,29 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy import SpotifyClientCredentials
+import time
 
 from colour_modes import Colours
 from light_strip_sim import LightStripSim
+from light_controller import PwmLedController, LightController
 from mood_based_colours import MoodBasedColours, Key
-
-import random
-import time
 
 features_thresholds = {"danceability": 0.6,
                        "energy": 0.5,
                        "valence": 0.6}
 
 LOOP_DELAY = 5 # s
-USE_SIM = True
+USE_SIM = False
+PWM_LED = True
+NUM_LEDS = 1
 
 if USE_SIM:
-    sim = LightStripSim(1)
-colours = Colours(1, 0, 255)
+    controller = LightStripSim(NUM_LEDS)
+elif PWM_LED:
+    controller = PwmLedController()
+else:
+    controller = LightController()
+colours = Colours(NUM_LEDS, 0, 255)
 mood_colours = MoodBasedColours(features_thresholds)
 
 scope = "playlist-read-private user-read-currently-playing"
@@ -43,7 +48,7 @@ while True:
         # call the api again for the accurate timing
         current_track = sp.current_user_playing_track()
         time_in = current_track["progress_ms"] / 1000
-        sim.play_led_output(led_outs, time_in)
+        controller.play_led_output(led_outs, time_in)
     else:
         print("Nothing playing at the moment")
 
